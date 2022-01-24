81613e04706426f7bad433626a09d9a360f53278
Zachary Atkins <zatkins2@gmail.com>
2022-01-24 10:50:53 -0500

    small fsaw edits

--- mnms/fsaw_noise.py
+++ mnms/fsaw_noise.py
@@ -8,6 +8,54 @@ from scipy.special import comb
 from scipy.interpolate import interp1d
 
 
+class FSAWKernels:
+
+    def __init__(self, lamb, lmax, lmin, lmax_j, n, full_shape, full_wcs,
+                 dtype=np.float32, iso_low=True, iso_high=True):
+        self._kf = KernelFactory(lamb, lmax, lmin, lmax_j, n, full_shape,
+                                 full_wcs, dtype=dtype, iso_low=iso_low,
+                                 iso_high=iso_high)
+
+        # get all my kernels -- radial ordering
+        self._kernels = {}
+        for i in range(len(self._kf._rad_funcs)):
+            if (i == 0 and iso_low) or (i == len(self._kf._rad_funcs)-1 and iso_high):
+                self._kernels[i, 0] = self._kf.get_kernel(i, 0)
+            else:
+                for j in range(n+1):
+                    self._kernels[i, j] = self._kf.get_kernel(i, j)
+        self._num_kernels = len(self._kernels)
+
+    def k2wav(self, kmap):
+        wavs = {}
+        for kern_key, kernel in self._kernels.items():
+            wavs[kern_key] = kernel.k2wav(kmap, from_full=True)
+        return wavs
+
+    def wav2k(self, wavs):
+        # first get new kmap
+        restype = np.result_type(1j*np.zeros(1, dtype=self._kf._dtype))
+        kmap = enmap.zeros(
+            self._kf._real_shape, wcs=self._kf._full_wcs, dtype=restype
+            )
+
+        # extract each kernel and insert
+        for kern_key, kernel in self._kernels.items():
+            kmap_wav = kernel.wav2k(wavs[kern_key])
+            for sel in kernel._sels:
+                kmap[sel] += kmap_wav[sel]
+
+        return kmap
+
+    @property
+    def kernels(self):
+        return self._kernels
+
+    @property
+    def num_kernels(self):
+        return self._num_kernels
+
+
 class Kernel:
 
     def __init__(self, k_kernel, sels=None, modlmap2=None, nthread=0, plot_wcs=None):
@@ -89,9 +137,8 @@ class KernelFactory:
 
     def __init__(self, lamb, lmax, lmin, lmax_j, n, full_shape, full_wcs,
                  dtype=np.float32, iso_low=True, iso_high=True):
-        w_ells, lmaxs = wlm_utils.get_sd_kernels(
-            lamb, lmax, lmin=lmin, lmax_j=lmax_j
-            )
+
+        w_ells, lmaxs = wlm_utils.get_sd_kernels(lamb, lmax, lmin=lmin, lmax_j=lmax_j)
         assert w_ells.ndim == 2, f'w_ell must be a 2d array, got {w_ells.ndim}d'
 
         # get list of w_ell callables
@@ -134,7 +181,7 @@ class KernelFactory:
         self._real_shape = (full_shape[0], full_shape[1]//2+1)
         self._full_wcs = full_wcs
         self._dtype = dtype
-        self._modlmap = enmap.modlmap(self._full_shape, full_wcs).astype(dtype, copy=False)[..., :self._real_shape[1]]
+        self._modlmap = enmap.modlmap(full_shape, full_wcs).astype(dtype, copy=False)[..., :self._real_shape[1]]
         self._phimap = np.arctan2(*enmap.lrmap(full_shape, full_wcs), dtype=dtype)
         assert self._modlmap.shape == self._phimap.shape, \
             'modlmap and phimap have different shapes'
@@ -165,6 +212,8 @@ class KernelFactory:
         y_sum = real_kern.sum(axis=1).astype(bool)
         y_sum_pos = np.nonzero(y_sum == False)[0]
         y_sum_neg = np.nonzero(y_sum[::-1] == False)[0]
+
+        # either both or neither are empty
         assert not np.logical_xor(y_sum_pos.size == 0, y_sum_neg.size == 0), \
             '0-cuts in y-direction of kernel must occur in both +y and -y'
         
@@ -175,7 +224,7 @@ class KernelFactory:
             y_max_pos = y_sum_pos.min() + 1 # for safety
             y_max_neg = y_sum_neg.min() + 1 # for safety
             
-            # assert we did everything right
+            # we did everything right if there is 1 more in y than x
             assert y_max_pos == y_max_neg + 1, \
                 f'y_max_pos and y_max_neg must differ in absval by 1, ' \
                 f'got {y_max_pos} and {y_max_neg}'
@@ -190,8 +239,10 @@ class KernelFactory:
             kern = real_kern
             modlmap2 = self._modlmap**2
         else:
+            # if y's are full but x's are clipped, then only need to slice in x
             if (kern_shape[0] == self._real_shape[0]) and (kern_shape[1] != self._real_shape[1]):
                 sels = [np.s_[..., :x_max],]
+            # if y's are clipped, need to slice in y, and :x_max will work whether clipped or not
             else:
                 sels = [np.s_[..., :y_max_pos, :x_max], np.s_[..., -y_max_neg:, :x_max]]
             kern = np.empty(kern_shape, dtype=real_kern.dtype)
@@ -200,55 +251,9 @@ class KernelFactory:
                 kern[sel] = real_kern[sel]
                 modlmap2[sel] = self._modlmap[sel]**2
 
-        pos = enmap.corners(self._full_shape, self._full_wcs, corner=False)
-        _, plot_wcs = enmap.geometry(pos, shape=kern_shape)
+        plot_pos = enmap.corners(self._full_shape, self._full_wcs, corner=False)
+        # kernels have 2(rkx-1)+1 pixels in map space x direction
+        plot_shape = (kern_shape[0], 2*(kern.shape[1]-1) + 1)
+        _, plot_wcs = enmap.geometry(plot_pos, plot_shape) 
 
         return Kernel(kern, sels=sels, modlmap2=modlmap2, plot_wcs=plot_wcs)
-
-
-class FSAWKernels:
-
-    def __init__(self, lamb, lmax, lmin, lmax_j, n, full_shape, full_wcs,
-                 dtype=np.float32, iso_low=True, iso_high=True):
-        self._kf = KernelFactory(lamb, lmax, lmin, lmax_j, n, full_shape,
-                                 full_wcs, dtype=dtype, iso_low=iso_low,
-                                 iso_high=iso_high)
-
-        # get all my kernels -- radial ordering
-        self._kernels = {}
-        for i in range(len(self._kf._rad_funcs)):
-            if (i == 0 and iso_low) or (i == len(self._kf._rad_funcs)-1 and iso_high):
-                self._kernels[i, 0] = self._kf.get_kernel(i, 0)
-            else:
-                for j in range(n+1):
-                    self._kernels[i, j] = self._kf.get_kernel(i, j)
-        self._num_kernels = len(self._kernels)
-
-    def k2wav(self, kmap):
-        wavs = {}
-        for kern_key, kernel in self._kernels.items():
-            wavs[kern_key] = kernel.k2wav(kmap, from_full=True)
-        return wavs
-
-    def wav2k(self, wavs):
-        # first get new kmap
-        restype = np.result_type(1j*np.zeros(1, dtype=self._kf._dtype))
-        kmap = enmap.zeros(
-            self._kf._real_shape, wcs=self._kf._full_wcs, dtype=restype
-            )
-
-        # extract each kernel and insert
-        for kern_key, kernel in self._kernels.items():
-            kmap_wav = kernel.wav2k(wavs[kern_key])
-            for sel in kernel._sels:
-                kmap[sel] += kmap_wav[sel]
-
-        return kmap
-
-    @property
-    def kernels(self):
-        return self._kernels
-
-    @property
-    def num_kernels(self):
-        return self._num_kernels
