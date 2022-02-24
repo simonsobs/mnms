2c05502477e3a5c294a95f1cdacde72e720f7bc8
Zachary Atkins <zatkins2@gmail.com>
2022-02-24 18:02:20 -0500

    real ffts in filtering

--- mnms/utils.py
+++ mnms/utils.py
@@ -1756,37 +1756,79 @@ def pickup_filter(imap, vk_mask=None, hk_mask=None):
 # copied from https://github.com/amaurea/tenki/blob/master/filter_pickup.py,
 # commit 2ddafa9; don't want tenki dependencies
 def build_filter(shape, wcs, lbounds, dtype=np.float32):
+    """Build a (real-) Fourier-space filter that selects a "Gaussian"
+    window of modes, with 2-sigmas (full-width at sigma) in ly, lx
+    given by lbounds.
+
+    Parameters
+    ----------
+    shape : iterable
+        Shape of the real-space map.
+    wcs : astropy.wcs.WCS
+        Wcs of map geometry to be downgraded.
+    lbounds : (2,) iterable
+        The ly, lx widths.
+    dtype : dtype, optional
+        Data type of filter, by default np.float32.
+    
+    Returns
+    -------
+    enmap.ndmap
+        Real-Fourier-space filter. This should be 1 for good modes,
+        and 0 for bad modes.
+    """
+    assert len(lbounds) == 2, f'Can only supply 2 lbounds, got {len(lbounds)}'
+
 	# Intermediate because the filter we're applying is for a systematic that
 	# isn't on the curved sky.
-	ly, lx  = enmap.laxes(shape, wcs, method="intermediate")
-	ly, lx  = [a.astype(dtype) for a in [ly,lx]]
-	lbounds = np.asarray(lbounds).astype(dtype)
-	if lbounds.ndim < 2:
-		lbounds = np.broadcast_to(lbounds, (1,2))
-	if lbounds.ndim > 2 or lbounds.shape[-1] != 2:
-		raise ValueError("lbounds must be [:,{ly,lx}]")
-	filter = enmap.ones(shape[-2:], wcs, dtype)
-	# Apply the filters
-	for i , (ycut, xcut) in enumerate(lbounds):
-		filter *= 1-(np.exp(-0.5*(ly/ycut)**2)[:,None]*np.exp(-0.5*(lx/xcut)**2)[None,:])
-	return filter
-
-def filter_simple(imap, filter):
-	return enmap.ifft(enmap.fft(imap)*filter).real
+    ly, lx  = enmap.laxes(shape, wcs, method="intermediate")
+    lx = lx[:lx.size//2+1] # real signal
+    ly, lx  = [a.astype(dtype, copy=False) for a in [ly,lx]]
+    f = enmap.ones((shape[-2], shape[-1]//2+1), wcs, dtype)
+    # Apply the filters
+    ycut, xcut = lbounds
+    f *= 1 - (np.exp(-0.5*(ly/ycut)**2)[:, None] * np.exp(-0.5*(lx/xcut)**2)[None, :])
+    return f
 
 def filter_weighted(imap, ivar, filter, tol=1e-4, ref=0.9):
-	"""Filter enmap imap with the given 2d fourier filter while
-	weithing spatially with ivar"""
-	filter = 1-filter
-	omap   = enmap.ifft(filter*enmap.fft(imap*ivar)).real
-	div    = enmap.ifft(filter*enmap.fft(     ivar)).real
-	del filter
-	# Avoid division by very low values
-	div    = np.maximum(div, np.percentile(ivar[::10,::10],ref*100)*tol)
-	# omap = imap - rhs/div
-	omap /= div
-	del div
-	omap *= -1
-	omap += imap
-	omap *= imap != 0
-	return omap
+    """Filter imap with the given 2D (real-) Fourier-space filter,
+    while weighing spatially with ivar.
+
+    Parameters
+    ----------
+    imap : enmap.ndmap
+        Map to filter.
+    ivar : enmap.ndmap
+        Spatial weights to apply to imap, i.e. more filtering will
+        occur where imap is larger.
+    filter : array-like
+        Real-Fourier-space filter. This should be 1 for good modes,
+        and 0 for bad modes. 
+    tol : float, optional
+        Relative size of maximum ivar divisor, by default 1e-4.
+    ref : float, optional
+        Percentile of maximum ivar divisor, by default 0.9
+
+    Returns
+    -------
+    enmap.ndmap
+        The filtered map.
+
+    Notes
+    -----
+    To avoid dividing by small values, the divisor is capped at 
+    np.percentile(ivar[::10, ::10], ref*100) * tol
+    """
+    filter = 1 - filter
+    omap = irfft(filter * rfft(imap * ivar))
+    div = irfft(filter * rfft(ivar))
+    filter = None
+    # Avoid division by very low values
+    div = np.maximum(div, np.percentile(ivar[::10, ::10], ref*100) * tol)
+    # omap = imap - rhs/div
+    omap /= div
+    del div
+    omap *= -1
+    omap += imap
+    omap *= imap != 0
+    return omap
